4a66278d648bde1cb42e791117b0d90648737109
franco galbiati <franco.galbiati@finniu.com>
2024-12-21 14:03:50 -0300

    add ( accordion ) : add accordion components

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,39 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
 export default function Page() {
+
+    const items = [
+        {
+            id: 1,
+            title: "Is it accessible?",
+            content: "Yes. It adheres to the WAI-ARIA design pattern.",
+        },
+        {
+            id: 2,
+            title: "Is it styled?",
+            content: "Yes. It comes with default styles that matches the other components' aesthetic.",
+        },
+        {
+            id: 3,
+            title: "Is it animated?",
+            content: "Yes. It's animated by default, but you can disable it if you prefer.",
+        }
+    ];
+
     return (
         <div>
-            <h2>Accordion</h2>
+            <Accordion type="single" collapsible className="w-full">
+                {
+                    items.map((item) => (
+                        <AccordionItem value={`item-${item.id}`} key={item.id}>
+                            <AccordionTrigger>{item.title}</AccordionTrigger>
+                            <AccordionContent>
+                                {item.content}
+                            </AccordionContent>
+                        </AccordionItem>
+                    ))
+                }
+            </Accordion>
         </div>
     );
 }
