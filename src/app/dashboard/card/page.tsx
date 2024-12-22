85361d06407c07ad524baf7e9a7dd1e0d3579ab3
franco galbiati <franco.galbiati@finniu.com>
2024-12-22 15:39:33 -0300

    add ( card )

---
+++ src/app/dashboard/card/page.tsx
@@ -0,0 +1,42 @@
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+
+
+
+export default function Page() {
+    return (
+        <div className="flex flex-wrap gap-2 justify-center">
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card content</p>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Button variant="outline">Cancel</Button>
+                    <Button>Deploy</Button>
+                </CardFooter>
+            </Card>
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card content</p>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Button variant="outline">Cancel</Button>
+                    <Button>Deploy</Button>
+                </CardFooter>
+            </Card>
+
+        </div>
+    );
+}
+
+
