4c7b26a0813e51782bf9a561a71e9f25058554b7
franco galbiati <franco.galbiati@finniu.com>
2024-12-22 15:43:43 -0300

    add ( add card implements )

--- src/app/dashboard/card/page.tsx
+++ src/app/dashboard/card/page.tsx
@@ -21,22 +21,36 @@ export default function Page() {
                     <Button>Deploy</Button>
                 </CardFooter>
             </Card>
-            <Card className="w-[350px]">
-                <CardHeader>
-                    <CardTitle>Create project</CardTitle>
-                    <CardDescription>Deploy your new project in one-click.</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>Card content</p>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    <Button variant="outline">Cancel</Button>
-                    <Button>Deploy</Button>
-                </CardFooter>
-            </Card>
+            <CardImplement title="Create project" description="Deploy your new project in one-click." content="Card content" buttonCancel="Cancel" buttonAction="Deploy" />
 
         </div>
     );
+}
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+    title: string
+    description: string
+    content: string
+    buttonCancel: string
+    buttonAction: string
+}
+
+const CardImplement = ({ title, description, content, buttonCancel, buttonAction }: CardProps) => {
+    return (
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>{title}</CardTitle>
+                <CardDescription>{description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>{content}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button variant="outline">{buttonCancel}</Button>
+                <Button>{buttonAction}</Button>
+            </CardFooter>
+        </Card>
+    )
 }
 
 
